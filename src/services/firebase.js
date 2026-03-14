7c8fb3e4adc7f971fc4127255655369c201e6bbf
mystique-entity <sheebashinob2005@gmail.com>
2026-03-14 13:24:48 +0630

    inetrfase firebase connect

---
+++ src/services/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBP7-wv0iSxI7aZAPJ-EWLDoNUySt6o_yo",
+  authDomain: "smart-reminder-c0b89.firebaseapp.com",
+  projectId: "smart-reminder-c0b89",
+  storageBucket: "smart-reminder-c0b89.firebasestorage.app",
+  messagingSenderId: "16660804934",
+  appId: "1:16660804934:web:d13cd9c4166af3e0b42147",
+  measurementId: "G-FYT7JK1JMW"
+};
